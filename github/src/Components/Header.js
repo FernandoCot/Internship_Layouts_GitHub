2fe946ccf9cffa17eb89ea1b7337725302f70886
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-09 17:08:46 -0300

    Images Added, Started the Responsive Rules, Comments Added

--- github/src/Components/Header.js
+++ github/src/Components/Header.js
@@ -5,31 +5,38 @@ class Header extends React.Component{
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className="navbar-brand">
-                    <i class="fab fa-github"></i>
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav m-auto">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Features</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Business</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Explore</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Marketplace</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Pricing</Link>
-                        </li>
-                    </ul>
-                    <input placeholder="Search GitHub"></input>
+                <div className="row navbar_main_row">
+                    <Link to="/" className="navbar-brand">
+                        <i class="fab fa-github github_icon"></i>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Features</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Business</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Explore</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Marketplace</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Pricing</Link>
+                            </li>
+                        </ul>
+                        <input className="navbar_search no_mobile" placeholder="Search GitHub"></input>
+                        <h5 class="no_mobile">
+                            <b className="font_white">Sign in</b>
+                            or 
+                            <b className="font_white">Sign up</b>
+                        </h5>
+                    </div>
                 </div>
             </nav>
         )
