2bf76da26967c81bfe77b1aec359aaabca2e0577
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-17 18:11:46 -0300

    Console.Log Tests, Dependencies Updated and CSS Fixes

--- github/src/Components/UserPage/UserMenu.js
+++ github/src/Components/UserPage/UserMenu.js
@@ -1,20 +1,51 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-class UserMenu extends React.Component{    
+class UserMenu extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            menu: [],
+        }
+    }
+    
+    componentDidMount(){
+        
+        fetch('https://api.github.com/users/FernandoCot')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    menu: [json],        
+                })
+            });
+    }
     
     render() {
+
+        var { menu } = this.state;
+
         return(
             <div className="col-9 text-left pl-2">
-                <div>
-                    <nav className="navbar_user">
-                        <Link to="/" className="user_nav_item">Overview</Link>
-                        <Link to="/repo" className="user_nav_item">Repositories</Link>
-                        <Link to="/stars" className="user_nav_item">Stars</Link>
-                        <Link to="/followers" className="user_nav_item">Followers</Link>
-                        <Link to="/following" className="user_nav_item">Following</Link>
-                    </nav>
-                </div>
+                {menu.map(json => (
+                    <div key={json.id}>
+                        <nav className="navbar_user">
+                            <Link to="/" className="user_nav_item">Overview</Link>
+                            <Link to="/repo" className="user_nav_item">Repositories
+                                <span className="counter">{json.public_repos}</span>
+                            </Link>
+                            <Link to="/stars" className="user_nav_item">Stars
+                                <span className="counter">2</span>
+                            </Link>
+                            <Link to="/followers" className="user_nav_item">Followers
+                                <span className="counter">{json.followers}</span>
+                            </Link>
+                            <Link to="/following" className="user_nav_item">Following
+                                <span className="counter">{json.following}</span>
+                            </Link>
+                        </nav>
+                    </div>
+                ))}    
             </div>
         )
     }
