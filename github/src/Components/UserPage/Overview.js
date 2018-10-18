0029d54ce525b1cdbde2d57085855b6fe237e3ad
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-18 01:14:52 -0300

    Full Repo Request Fixed and CSS Minor Fixes

--- github/src/Components/UserPage/Overview.js
+++ github/src/Components/UserPage/Overview.js
@@ -15,7 +15,7 @@ class Overview extends React.Component{
             .then(res => res.json())
             .then(json => {
                 this.setState({
-                    overview: [json[0]],        
+                    overview: json,        
                 })
             });
     }
@@ -25,16 +25,25 @@ class Overview extends React.Component{
         var { overview } = this.state;
 
         return(
-            <div className="container">
+            <div className="mt-4">
+                <div className="container pad0">
+                    <div className="col-6 text-left pad0">
+                        <h2 className="popular_repo">Popular repositories</h2>
+                    </div>
+                    <div className="col-6 text-right pad0">
+                        <span className="pinned_repo">Customize your pinned repositories</span>
+                    </div>
+                </div>
                 {overview.map(json => (
-                    <div key={json.id}>
-                        <div className="col-lg-6 col-md-6 col-12 overview_div">
-                            <span className="tit_overview_repo">{json.name}</span>
-                            <p className="txt_overview_repo">{json.description}</p>
-                            <p className="">
+                    <div key={json.id} className="col-lg-6 col-md-6 col-12 overview_div">
+                        <span className="tit_overview_repo">{json.name}</span>
+                        <p className="txt_overview_repo">{json.description}</p>
+                        <p className="box_circle_language">
+                            <span className="language_circle_color"></span>
+                            <p className="language_overview">
                                 {json.language}
                             </p>
-                        </div>
+                        </p>
                     </div>
                 ))}
             </div>
