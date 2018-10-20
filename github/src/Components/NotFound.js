05901c5fadec174ab1df17ff7ba3f167712bcdd8
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-20 14:29:59 -0300

    OctoCat Imported, Component "NotFound" Updated and CSS Minor Fixes

--- github/src/Components/NotFound.js
+++ github/src/Components/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import img_big from '../img/Octocat.png'
 
 class NotFound extends React.Component{
     render(){
         return(
-            <h1 className="txt-center m-5 text-primary">Página Não Encontrada</h1>
+            <div className="div_not_found">
+                <h1 className="txt-center m-3">Error 404: Page Not Found!</h1>
+                <h3>Please check the URL adress and/or the respective parameters.</h3>
+                <img src={img_big} className="img_octo_cat" />
+            </div>
         )
     };
 }
