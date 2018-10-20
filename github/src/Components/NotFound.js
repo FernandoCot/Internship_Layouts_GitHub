ecd7b4ea77fdb0e8f25d23693ef7d39fc52d1df0
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-20 14:42:13 -0300

    Alterations in the "NotFound" Component and Styles

--- github/src/Components/NotFound.js
+++ github/src/Components/NotFound.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import img_big from '../img/Octocat.png'
+import img_octo_cat from '../img/Octocat.png'
 
 class NotFound extends React.Component{
     render(){
         return(
             <div className="div_not_found">
                 <h1 className="txt-center m-3">Error 404: Page Not Found!</h1>
-                <h3>Please check the URL adress and/or the respective parameters.</h3>
-                <img src={img_big} className="img_octo_cat" />
+                <h3 className="txt_not_found">Please check the URL address and/or the respective parameters.</h3>
+                <img src={img_octo_cat} className="img_octo_cat" alt="Octo_Cat"/>
             </div>
         )
     };
