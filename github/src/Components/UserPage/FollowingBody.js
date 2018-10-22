8f53e01119b52118b3b1e8ea7132894af3f19369
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-21 21:34:51 -0300

    Responsive Minor Fixes

--- github/src/Components/UserPage/FollowingBody.js
+++ github/src/Components/UserPage/FollowingBody.js
@@ -28,15 +28,15 @@ class FollowingBody extends React.Component{
             <div className="">
                 {FollowingBody.map(json => (
                     <div className="div_each_follower">
-                        <div className="col-1 p-0">
+                        <div className="col-3 col-lg-1 p-0">
                             <img src={json.avatar_url} className="img_follow" alt="Follower_Profile_Pic"></img>
                         </div>
-                        <div className="col-9 p-0">
+                        <div className="col-6 col-lg-9 p-0">
                             <h3 className="tit_follow">{json.login}</h3>
                             <h4 className="txt_follow">Description Test</h4>
                             <svg className="octicon octicon-location" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
                         </div>
-                        <div className="col-2 p-0 text-right">
+                        <div className="col-3 col-lg-2 p-0 text-right">
                             <button className="btn_unfollow">Unfollow</button>
                         </div>
                     </div>
