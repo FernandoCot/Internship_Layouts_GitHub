d02913959351a40ca566b300000fc8ab87e941dd
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-10-22 18:40:39 -0300

    Started Module "Circle" within the Integrations.js

--- github/src/Components/HomePage/Integrations.js
+++ github/src/Components/HomePage/Integrations.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Slack from "../../img/slack.png";
+import Zenhub from "../../img/zenhub.png";
+import Travis from "../../img/travis-ci.png";
+import Atom from "../../img/atom.png";
+import Circle from "../../img/circle-ci.png";
+import Google from "../../img/google.png";
+import CodeClimate from "../../img/codeclimate.png";
 
 class Integrations extends React.Component{
     render() {
+
+        var circle1 = { backgroundColor: '#553958'};
+        var circle2 = { backgroundColor: '#364e98'};
+        var circle3 = { backgroundColor: '#eff9f9'};
+        var circle4 = { backgroundColor: '#5fb57d'};
+        var circle5 = { backgroundColor: '#022531'};
+        var circle6 = { backgroundColor: '#f2f2f2'};
+        var circle7 = { backgroundColor: '#303030'};
+
         return(
             <section className="Section_Integrations">
                 <div className="container text-center">
@@ -11,6 +27,37 @@ class Integrations extends React.Component{
                         Customize your process with GitHub apps and an intuitive API. Integrate the tools you already use or discover new favorites to create a happier, more efficient way of working.
                     </h3>
                     <button className="btn_work_together">Learn about integrations →</button>
+                </div>
+                <div className="container div_logo_group">
+                    <div style={circle1} className="div_logo_circle">
+                        <img src={Slack} className="logo_circle" alt="Logo Slack"></img>
+                    </div>
+                    <div style={circle2} className="div_logo_circle">
+                        <img src={Zenhub} className="logo_circle" alt="Logo Zenhub"></img>
+                    </div>
+                    <div style={circle3} className="div_logo_circle">
+                        <img src={Travis} className="logo_circle" alt="Logo Travis"></img>
+                    </div>
+                    <div style={circle4} className="div_logo_circle">
+                        <img src={Atom} className="logo_circle" alt="Logo Atom"></img>
+                    </div>
+                    <div style={circle5} className="div_logo_circle">
+                        <img src={Circle} className="logo_circle" alt="Logo Circle"></img>
+                    </div>
+                    <div style={circle6} className="div_logo_circle">
+                        <img src={Google} className="logo_circle" alt="Logo Google"></img>
+                    </div>
+                    <div style={circle7} className="div_logo_circle">
+                        <img src={CodeClimate} className="logo_circle" alt="Logo Code Climate"></img>
+                    </div>
+                </div>
+                <div>
+                    <p className="txt_circle">
+                        Sometimes, there’s more than one tool for the job. Why not try something new?
+                    </p>
+                    <a href="https://github.com/marketplace" className="link_circle" target="_blank" rel="noopener noreferrer">
+                        Browse GitHub Marketplace <span>→</span>
+                    </a>
                 </div>
             </section>
         )
